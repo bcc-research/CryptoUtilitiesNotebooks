39dcad1bb4a7578fc874439c5c71a9e958f244f1
Andrija <akinovak@gmail.com>
2025-04-12 00:08:42 +0200

    Reed-Solomon walkthrough

--- 04_reedsolomon.jl
+++ 04_reedsolomon.jl
@@ -18,9 +18,19 @@ using BinaryFields, BinaryReedSolomon
 # ╔═╡ bbdb3e6a-50d1-46df-9c40-77cd0535d019
 using BenchmarkTools
 
+# ╔═╡ 28a8669c-a64f-46a9-a864-f103a8beb99a
+md"""
+# BinaryReedSolomon.jl
+
+CryptoUtilities also come with a BinaryReedSolomon package.
+This package provides an API for manipulating Reed-Solomon codes over binary extension fields. 
+
+As before, unfortunately, we have to manually add the package until we finish adding it to the Julia package registry!
+"""
+
 # ╔═╡ bbbb51f5-1798-4d73-b7f5-c0568c4a1dfa
 md"""
-Now let's initialize ReedSolomon code over $F_{2^{16}}$
+Now let's initialize Reed-Solomon code over $F_{2^{16}}$ with message length 2^10 and rate 1/4. 
 """
 
 # ╔═╡ 71b5f2d2-3c54-4815-8ea7-a2f9947d7ac4
@@ -58,7 +68,7 @@ Let's start by importing the `BenchmarkTools` package
 
 # ╔═╡ 5a90c671-f691-43c2-8cd2-8d442714bc7f
 md"""
-XXX
+Now we work with much longer messages, of size 2^20, and rate again 1/4 which gives us block length of 2^22. Reed–Solomon codes are defined only for block lengths $n \le q$, where $q$ is the size of the field. Therefore, we cannot stay in $F_{2^{16}}$ and thus we switch to $F_{2^{32}}$.
 """
 
 # ╔═╡ 45495c3e-bb9a-4743-91c6-9300117cd315
@@ -77,9 +87,10 @@ end
 md"In the next notebook we proceed to build a Ligero proving system on top of CryptoUtilities.jl"
 
 # ╔═╡ Cell order:
+# ╟─28a8669c-a64f-46a9-a864-f103a8beb99a
 # ╠═972386be-16f2-11f0-36c5-fbe5da0445d9
 # ╠═33c89c91-0ef4-4986-95e8-d1f65482fd91
-# ╟─bbbb51f5-1798-4d73-b7f5-c0568c4a1dfa
+# ╠═bbbb51f5-1798-4d73-b7f5-c0568c4a1dfa
 # ╠═71b5f2d2-3c54-4815-8ea7-a2f9947d7ac4
 # ╠═afacf7b9-250e-4c42-a53a-191f6580310f
 # ╠═ebe614e4-47ac-44ad-9149-a639792d501d
